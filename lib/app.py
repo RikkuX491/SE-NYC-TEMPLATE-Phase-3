7445e9829383e8fcb91c7aca6386dc488b501886
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-01 10:55:19 -0400

    Created starter code and content for Lecture 2

---
+++ lib/app.py
@@ -0,0 +1,43 @@
+import ipdb
+
+def combine_sequences(seq1, seq2):
+    pass
+
+def sequence_n_times(seq, n):
+    pass
+
+def average(seq):
+    pass
+
+def append_n_times(input_list, element, n):
+    pass
+
+foods = [
+    {
+        "name": "Flatburger",
+        "price": 10.99
+    },
+    {
+        "name": "French Fries",
+        "price": 1.99
+    },
+    {
+        "name": "Burrito",
+        "price": 7.99
+    }
+]
+
+animals = [
+    {
+        "name": "Fido",
+        "animal_type": "Dog"
+    },
+    {
+        "name": "Kitty",
+        "animal_type": "Cat"
+    },
+    {
+        "name": "Fluffy",
+        "animal_type": "Guinea Pig"
+    }
+]
