66a47d05a7a0238179001aa30ee1a59d5e687289
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-09 16:55:22 -0400

    Updated starter code and README.md

--- README.md
+++ README.md
@@ -12,16 +12,13 @@ In today's lecture, we will discuss more topics of Object Oriented Programming i
 ## Setup
 
 1. Make sure that your current working directory (folder) contains a `Pipfile`, then run `pipenv install` in your terminal to install `pytest` and any other required packages.
+
 2. Now that your `pipenv` virtual environment is ready to use, enter `pipenv shell` to enter the virtual environment.
 
 ## Deliverables
 
 Write your code in `lib/car.py` (the `car.py` file in the `lib` directory / folder)
 
-1. Create a property for the `model` instance variable. For the setter method, the `model` cannot be changed after it is initialized and must be a `str`. Otherwise, raise an `Exception` with the message "Model cannot be changed and must be a string!" Hint: Use `has_attr()`.
+1. Create a class attribute, `all`. We will use this attribute to keep track of the new `Car` instances that are created from the `Car` class. Set this attribute equal to `[]`. Then in `__init__`, you will need to append the new `Car` instances to the list contained within the `all` class attribute for the `Car` class.
 
-2. Create a class attribute, `all`. We will use this attribute to keep track of
-the new `Car` instances that are created from the `Car` class. Set this
-attribute equal to `[]`. Then in `__init__`, you will need to append the new `Car` instances to the list contained within the `all` class attribute for the `Car` class.
-
-3. Create a class method, `average_year()` that will return the average year for all cars created.
+2. Create a class method, `average_year()` that will return the average year for all cars created.
--- lib/car.py
+++ lib/car.py
@@ -2,44 +2,61 @@ import ipdb
 
 class Car:
     
-    def __init__(self, make, model, year, horn_volume = 1):
+    def __init__(self, make, model, year, horn_volume=1):
         self.make = make
         self.model = model
         self.year = year
         self.horn_volume = horn_volume
 
     @property
+    def make(self):
+        return self._make
+    
+    @make.setter
+    def make(self, value):
+        if not (type(value) == str):
+            raise TypeError("Make must be a string!")
+        elif len(value) < 3:
+            raise ValueError("Make must be at least 3 characters long!")
+        else:
+            self._make = value
+
+    @property
+    def model(self):
+        return self._model
+    
+    @model.setter
+    def model(self, value):
+        if (hasattr(self, 'model')) or not (type(value) == str):
+            raise Exception("Model cannot be changed and must be a string!")
+        else:
+            self._model = value
+
+    @property
     def year(self):
         return self._year
     
     @year.setter
-    def year(self, year_parameter):
-        if(isinstance(year_parameter, int)) and (1900 <= year_parameter <= 2023):
-            self._year = year_parameter
+    def year(self, value):
+        if not (type(value) == int):
+            raise TypeError("Year must be an integer!")
+        elif not (1900 <= value <= 2024):
+            raise ValueError("Year must be between 1900 and 2024!")
         else:
-            raise ValueError("Year must be an integer and must be between 1900 and 2023!")
-    
+            self._year = value
+
     @property
     def horn_volume(self):
         return self._horn_volume
     
     @horn_volume.setter
-    def horn_volume(self, horn_volume_parameter):
-        if(isinstance(horn_volume_parameter, int)):
-            self._horn_volume = horn_volume_parameter
+    def horn_volume(self, value):
+        if not (type(value) == int):
+            raise TypeError("Horn Volume must be an integer!")
+        elif not (1 <= value <= 10):
+            raise ValueError("Horn Volume must be between 1 and 10!")
         else:
-            raise ValueError("Horn volume must be an integer!")
-        
-    @property
-    def make(self):
-        return self._make
-    
-    @make.setter
-    def make(self, make_parameter):
-        if(type(make_parameter) == str) and (len(make_parameter) >= 3):
-            self._make = make_parameter
-        else:
-            raise ValueError("Make must be a string and must be at least 3 characters long!")
-        
+            self._horn_volume = value
+
     def honk_horn(self):
         print(f"BEEP BEEP{'!' * self.horn_volume}")
