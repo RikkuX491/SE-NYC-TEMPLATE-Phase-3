34a8877e2253349be379176c2bf57d3794f4a86e
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-12 11:04:16 -0500

    Completed SinglyLinkedList class starter code

--- lib/data_structure_examples/singly_linked_list.py
+++ lib/data_structure_examples/singly_linked_list.py
@@ -3,6 +3,18 @@ class SinglyLinkedList:
     def __init__(self, head=None):
         self.head = head
 
+    def append(self, node):
+        # Add element to the beginning of the list if the list is empty
+        if self.head == None:
+            self.head = node
+            return
+        
+        # Otherwise, traverse the list to find the last node
+        current_node = self.head
+        while current_node.next_node:
+            current_node = current_node.next_node
+        current_node.next_node = node
+
 # Since our SinglyLinkedList class is going to contain a series of nodes linked together, we'll create a Node class.
 class Node:
     def __init__(self, data, next_node = None):
