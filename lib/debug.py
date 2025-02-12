9f2803a86b6a4be9af1013e91facb77ff0cd93ed
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-12 11:12:04 -0500

    Added import statement to import SinglyLinkedList and Node classes into debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,7 @@ from models.__init__ import CONN, CURSOR
 from models.hotel import Hotel
 from models.customer import Customer
 from models.review import Review
+from data_structure_examples.singly_linked_list import SinglyLinkedList, Node
 
 if __name__ == '__main__':
     # don't remove this line, it's for debugging!
