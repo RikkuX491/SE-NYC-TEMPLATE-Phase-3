237d3df7ae5be0d718b47f5b87b11f44dbbeb030
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-04 15:00:19 -0500

    Updated comments in debug.lpy

--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,8 @@ from models.customer import Customer
 from models.review import Review
 # from data_structure_examples.singly_linked_list import SinglyLinkedList, Node
 # from data_structure_examples.doubly_linked_list import DoublyLinkedList, Node
+# from data_structure_examples.tree import Tree, Node
+# from data_structure_examples.binary_tree import BinaryTree, BinarySearchTree, Node
 
 if __name__ == '__main__':
     # don't remove this line, it's for debugging!
