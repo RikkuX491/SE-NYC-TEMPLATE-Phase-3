3fbcbe83306aef985141e9d50aaaadb1baae980b
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-04 14:56:49 -0500

    Added Tree, BinaryTree, and BinarySearchTree classes

---
+++ lib/data_structure_examples/binary_tree.py
@@ -0,0 +1,13 @@
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
---
+++ lib/data_structure_examples/tree.py
@@ -0,0 +1,8 @@
+class Tree:
+    def __init__(self):
+        self.root = None
+
+class Node:
+    def initialize(self, value):
+        self.value = value
+        self.children = []
